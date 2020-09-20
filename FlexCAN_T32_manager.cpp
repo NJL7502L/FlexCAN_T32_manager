608fd5d6c89ba4bfb1b206155f484f9aae4b165d
NJL7502L <g.hana1211.tako@gmail.com>
2020-09-20 20:16:08 +0900

    T32.hとT32.cppを追加

---
+++ FlexCAN_T32_manager.cpp
@@ -0,0 +1,211 @@
+#include <FlexCAN_T32.h>
+#include <FlexCAN_T32_manager.h>
+
+// CANインスタンス
+FlexCAN_T32<CAN1, RX_SIZE_256, TX_SIZE_16> Can1;
+FlexCAN_T32<CAN2, RX_SIZE_256, TX_SIZE_16> Can2;
+FlexCAN_T32<CAN3, RX_SIZE_256, TX_SIZE_16> Can3;
+
+//受信側maptable
+std::map<uint32_t, CAN_message_t> CAN1_mestable;
+std::map<uint32_t, CAN_message_t> CAN2_mestable;
+std::map<uint32_t, CAN_message_t> CAN3_mestable;
+
+//送信側vector
+std::vector<CAN_message_t> CAN1_senddata;
+std::vector<CAN_message_t> CAN2_senddata;
+std::vector<CAN_message_t> CAN3_senddata;
+
+//インスタンスを取得することができる唯一の関数
+FlexCAN_T32_manager *FlexCAN_T32_manager::getInstance() {
+  static FlexCAN_T32_manager canmanager;
+  return &canmanager; // canmanagerのインスタンスのアドレスを返す。
+}
+
+FlexCAN_T32_manager::FlexCAN_T32_manager() {}
+
+//初期化関数
+void FlexCAN_T32_manager::init(bool canbus1, bool canbus2, bool canbus3) {
+  if (canbus1) {
+    Can1.begin();
+    Can1.setBaudRate(1000000);
+    can1busisopen = true;
+  }
+  if (canbus2) {
+    Can2.begin();
+    Can2.setBaudRate(1000000);
+    can2busisopen = true;
+  }
+  if (canbus3) {
+    Can3.begin();
+    Can3.setBaudRate(1000000);
+    can3busisopen = true;
+  }
+  delay(500);
+  inited = true;
+}
+
+bool FlexCAN_T32_manager::isinit() { return inited; }
+// mapテーブルクリア関数
+void FlexCAN_T32_manager::clearAllTable() {
+  CAN1_mestable.clear();
+  CAN2_mestable.clear();
+  CAN3_mestable.clear();
+}
+// vectorテーブルクリア関数
+void FlexCAN_T32_manager::clearAllVector() {
+  CAN1_senddata.clear();
+  CAN2_senddata.clear();
+  CAN3_senddata.clear();
+}
+
+// canbus存在チェック関数
+bool FlexCAN_T32_manager::CAN1isOpen() { return can1busisopen; }
+bool FlexCAN_T32_manager::CAN2isOpen() { return can2busisopen; }
+bool FlexCAN_T32_manager::CAN3isOpen() { return can3busisopen; }
+
+// CAN受信データ途絶検知関数
+bool FlexCAN_T32_manager::isNoCan1() { return flgNoCan1Data; }
+bool FlexCAN_T32_manager::isNoCan2() { return flgNoCan2Data; }
+bool FlexCAN_T32_manager::isNoCan3() { return flgNoCan3Data; }
+
+//再始動関数
+void FlexCAN_T32_manager::restartCan() {
+  Can1.reset();
+  Can2.reset();
+  Can3.reset();
+  clearAllTable();
+  clearAllVector();
+  init(can1busisopen, can2busisopen, can3busisopen);
+}
+
+// canbusからデータを受信する関数
+void FlexCAN_T32_manager::readBus1() {
+  CAN_message_t CAN1msg;
+  static int can1Cnt;
+  while (Can1.read(CAN1msg)) {
+    CAN1_mestable[CAN1msg.id] = CAN1msg;
+    can1Cnt = 0;
+  }
+  // CAN受信エラーカウンタで分岐
+  if (can1Cnt == 0) { //正常受信
+    flgNoCan1Data = false;
+    can1Cnt += 1;
+  } else if (can1Cnt < 16) { //エラーだが許容範囲
+    can1Cnt += 1;
+  } else { // CANデータ受信途絶のとき緊急停止。
+    flgNoCan1Data = true;
+  }
+}
+
+void FlexCAN_T32_manager::readBus2() {
+  CAN_message_t CAN2msg;
+  static int can2Cnt;
+  while (Can2.read(CAN2msg)) {
+    CAN2_mestable[CAN2msg.id] = CAN2msg;
+    can2Cnt = 0;
+  }
+  // CAN受信エラーカウンタで分岐
+  if (can2Cnt == 0) { //正常受信
+    flgNoCan2Data = false;
+    can2Cnt += 1;
+  } else if (can2Cnt < 16) { //エラーだが許容範囲
+    can2Cnt += 1;
+  } else { // CANデータ受信途絶のとき緊急停止。
+    flgNoCan2Data = true;
+  }
+}
+
+void FlexCAN_T32_manager::readBus3() {
+  CAN_message_t CAN3msg;
+  static int can3Cnt;
+  while (Can3.read(CAN3msg)) {
+    CAN3_mestable[CAN3msg.id] = CAN3msg;
+    can3Cnt = 0;
+  }
+  // CAN受信エラーカウンタで分岐
+  if (can3Cnt == 0) { //正常受信
+    flgNoCan3Data = false;
+    can3Cnt += 1;
+  } else if (can3Cnt < 16) { //エラーだが許容範囲
+    can3Cnt += 1;
+  } else { // CANデータ受信途絶のとき緊急停止。
+    flgNoCan3Data = true;
+  }
+}
+// canbusからそれぞれのCAN1~3のデータを受信する
+void FlexCAN_T32_manager::readAll() {
+  if (can1busisopen) {
+    readBus1();
+  }
+  if (can2busisopen) {
+    readBus2();
+  }
+  if (can3busisopen) {
+    readBus3();
+  }
+}
+
+void FlexCAN_T32_manager::getBus1(uint32_t canid, uint8_t data[8]) {
+  CAN_message_t msg = CAN1_mestable[canid];
+  memcpy(data, msg.buf, 8);
+}
+void FlexCAN_T32_manager::getBus2(uint32_t canid, uint8_t data[8]) {
+  CAN_message_t msg = CAN2_mestable[canid];
+  memcpy(data, msg.buf, 8);
+}
+void FlexCAN_T32_manager::getBus3(uint32_t canid, uint8_t data[8]) {
+  CAN_message_t msg = CAN3_mestable[canid];
+  memcpy(data, msg.buf, 8);
+}
+
+// canmessageの生データをvectorに登録
+void FlexCAN_T32_manager::pushBus1(uint32_t canid, uint8_t buf[8]) {
+  CAN_message_t msg;
+  msg.id = canid;
+  for (int i = 0; i < 8; i++) {
+    msg.buf[i] = buf[i];
+  }
+  CAN1_senddata.push_back(msg);
+}
+void FlexCAN_T32_manager::pushBus2(uint32_t canid, uint8_t buf[8]) {
+  CAN_message_t msg;
+  msg.id = canid;
+  for (int i = 0; i < 8; i++) {
+    msg.buf[i] = buf[i];
+  }
+  CAN2_senddata.push_back(msg);
+}
+void FlexCAN_T32_manager::pushBus3(uint32_t canid, uint8_t buf[8]) {
+  CAN_message_t msg;
+  msg.id = canid;
+  for (int i = 0; i < 8; i++) {
+    msg.buf[i] = buf[i];
+  }
+  CAN3_senddata.push_back(msg);
+}
+
+// CANBusにデータを送信
+void FlexCAN_T32_manager::writeAll() {
+  // BUS 1
+  if (CAN1isOpen()) {
+    for (CAN_message_t outmes : CAN1_senddata) {
+      Can1.write(outmes);
+    }
+  }
+  CAN1_senddata.clear();
+  // BUS 2
+  if (CAN2isOpen()) {
+    for (CAN_message_t outmes : CAN2_senddata) {
+      Can2.write(outmes);
+    }
+  }
+  CAN2_senddata.clear();
+  // BUS 3
+  if (CAN3isOpen()) {
+    for (CAN_message_t outmes : CAN3_senddata) {
+      Can3.write(outmes);
+    }
+  }
+  CAN3_senddata.clear();
+}
---
+++ FlexCAN_T32_manager.h
@@ -0,0 +1,72 @@
+#ifndef FlexCAN_T32_manager_h
+#define FlexCAN_T32_manager_h
+#include <map>
+#include <vector>
+
+//シングルトン設計
+//シングルトン設計のためインスタンスを呼び出すときはポインタ変数として確保する。
+//代入演算子を使用した場合コピーされるのでシングルトンにならないので代入演算子、コピーコンストラクタの使用を禁止にしている。
+//使用方法はCANDEBUG.inoを参照のこと
+class FlexCAN_T32_manager {
+protected:
+  FlexCAN_T32_manager();
+
+  bool inited = false;
+  // CANbus init時にTrue
+  bool can1busisopen = false;
+  bool can2busisopen = false;
+  bool can3busisopen = false;
+  // CAN受信データ途絶時にTrue
+  bool flgNoCan1Data = true;
+  bool flgNoCan2Data = true;
+  bool flgNoCan3Data = true;
+
+  void operator=(const FlexCAN_T32_manager &obj) {}
+  FlexCAN_T32_manager(const FlexCAN_T32_manager &obj) {}
+
+  // mapテーブルクリア関数
+  void clearAllTable();
+  // vectorテーブルクリア関数
+  void clearAllVector();
+
+public:
+  //インスタンス操作
+  static FlexCAN_T32_manager *getInstance();
+
+  // init関連
+  void init(bool canbus1, bool canbus2, bool canbus3);
+  bool isinit();
+
+  // CAN受信データ途絶検知関数
+  bool isNoCan1();
+  bool isNoCan2();
+  bool isNoCan3();
+
+  void restartCan();
+
+  // CAN1メッセージ操作
+  bool CAN1isOpen(); // canがopenしているかどうか
+  void readBus1(); // canbusからデータを受け取ってhasmap上に格納する。
+  //格納されているデータ(buf)をCANIDを指定してとってくる。第２引数にはデータの送り先を指定する。
+  void getBus1(uint32_t canid, uint8_t data[8]);
+
+  // CAN2メッセージ操作
+  bool CAN2isOpen();
+  void readBus2();
+  void getBus2(uint32_t canid, uint8_t data[8]);
+
+  // CAN3メッセージ操作
+  bool CAN3isOpen();
+  void readBus3();
+  void getBus3(uint32_t canid, uint8_t data[8]);
+
+  //使用しているCANbusのデータを受信する関数
+  void readAll();
+
+  void pushBus1(uint32_t canid, uint8_t buf[8]);
+  void pushBus2(uint32_t canid, uint8_t buf[8]);
+  void pushBus3(uint32_t canid, uint8_t buf[8]);
+  // CANbusへの送信
+  void writeAll();
+};
+#endif
